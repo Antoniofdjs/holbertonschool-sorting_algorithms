d8e147325be9a035ffa2a879917a81e6f12e2797
Antoniofdjs <Antoniofdjs@gmail.com>
2024-01-13 13:32:52 -0400

    Selection sort algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * selection_sort- sorts array ints ascending order
+ * @array: array unsorted of ints
+ * @size: size of array
+ *
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int min;
+	size_t index_min = 0;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = array[i]; /* suppose first is always min */
+		for (j = i + 1; j < size; j++) /* start one index over and iterate all */
+		{
+			if (min > array[j])
+			{
+				min = array[j]; /* save new min */
+				index_min = j; /* remember index we found min */
+			}
+		}
+		if (index_swap != i) /* if no new min found, indx_min = i, wont execute */
+		{
+			array[index_min] = array[i]; /* switch value where indx min was found*/
+			array[i] = min; /* min set to lowest position in array available */
+			print_array(array, size);
+		}
+	}
+}
