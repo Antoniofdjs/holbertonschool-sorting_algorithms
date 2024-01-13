ddffd8061d54bf1e393b5fcf8c91c298acf76c49
Antoniofdjs <Antoniofdjs@gmail.com>
2024-01-13 13:44:59 -0400

    Initialized indx_min right after the first for to prevent errors

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,11 +11,12 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int min;
-	size_t index_min = 0;
+	size_t index_min;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min = array[i]; /* suppose first is always min */
+		min = array[i]; /* suppose i is always min when starting */
+		index_min = i;
 		for (j = i + 1; j < size; j++) /* start one index over and iterate all */
 		{
 			if (min > array[j])
